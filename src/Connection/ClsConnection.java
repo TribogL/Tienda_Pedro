47128d2471a1639281857157ff10dee2fd0f9a92
Tribog <julix4212@gmail.com>
2025-10-29 20:48:02 -0500

    Database connection.

--- src/Connection/ClsConnection.java
+++ src/Connection/ClsConnection.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Connection;
 
-/**
- *
- * @author MSI TIN
- */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
 public class ClsConnection {
-    
+
+    //Variable global
+    Connection CN;
+
+    public ClsConnection() {
+
+    }
+
+    public Connection Connect() {
+
+        try {
+            String URL, User, Password;
+            URL = "jdbc:mysqul://localhost:3306/dbtiendapedro";
+            User = "root";
+            Password ="";
+            
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            CN = DriverManager.getConnection(URL, User, Password);
+            if (CN != null){
+                JOptionPane.showMessageDialog(null, "Connection succecfull");
+            }
+        } catch (Exception e) {
+             JOptionPane.showMessageDialog(null, "Connection Error"+e.getLocalizedMessage());
+        }
+        return CN;
+    }
+
 }
